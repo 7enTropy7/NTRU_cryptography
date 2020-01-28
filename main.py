9dec9b316551f182a176ed7bab3fe5e69c8a3d7b
7enTropy7 <menon.uk1998@gmail.com>
2020-01-28 22:13:50 +0530

    circular_convolution added

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from Polynomial import Zx
 
+def circular_convolution(F,G,n): 
+    result = F.multiply(G)
+    t_ = []
+    for i in range(len(result.coeffs)):
+        if i>=n:
+            t_.append(result.coeffs[i])
+
+    result.coeffs = result.coeffs[0:3]
+
+    if len(result.coeffs)>len(t_):
+        for i in range(len(t_)):
+            result.coeffs[i]+=t_[i]
+    else:
+        for i in range(len(result.coeffs)):
+            result.coeffs[i]+=t_[i]
+    
+    return result
+
 F = Zx([3,1,4])
-G = Zx([2,7,1])
+G = Zx([0,1])
 print(F.print_polynomial())
 print(G.print_polynomial())
-result = F.multiply(G)
+result = circular_convolution(F,G,3)
 print(result.print_polynomial())
