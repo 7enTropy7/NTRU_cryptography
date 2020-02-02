e3fbe21ff3e30dc84c3fd5bb54ff4f83a05dcd2e
7enTropy7 <menon.uk1998@gmail.com>
2020-02-02 16:31:07 +0530

    invertmodpowerof2 added

--- main.py
+++ main.py
@@ -82,8 +82,8 @@ def invertmodprime(F,N,p):
 
 def Log2(x): 
     if x == 0: 
-        return false; 
-    return (math.log10(x)/math.log10(2)); 
+        return false 
+    return (math.log10(x)/math.log10(2)) 
   
 def isPowerOfTwo(n): 
     return (math.ceil(Log2(n)) == math.floor(Log2(n))); 
