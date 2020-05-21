7d0b33b98f3cea05413620fe334a6fcae2714c10
7enTropy7 <menon.uk1998@gmail.com>
2020-05-21 12:29:46 +0530

    minor optimization

--- character_wise_koblitz_encoding.py
+++ character_wise_koblitz_encoding.py
@@ -1,5 +1,6 @@
 import math
 from random import randint
+import numpy as np
 #import matplotlib.pyplot as plt
 
 def sq_root_mod_n(n, p):   
@@ -63,6 +64,22 @@ def primitive_start_point(N):
 
   return decoded_points
 
+def cantor_pair(k1, k2, safe=True):
+    z = int(0.5 * (k1 + k2) * (k1 + k2 + 1) + k2)
+    if safe and (k1, k2) != cantor_unpair(z):
+        raise ValueError("{} and {} cannot be paired".format(k1, k2))
+    return z
+
+
+def cantor_unpair(z):
+    w = np.floor((np.sqrt(8 * z + 1) - 1) / 2)
+    t = (w**2 + w) / 2
+    y = int(z - t)
+    x = int(w - y)
+    # assert z != pair(x, y, safe=False):
+    return x, y
+
+
 def dec_ternary(n):
     if n == 0:
         return '0'
@@ -86,4 +103,4 @@ print(ternary_dec([1,0,2]))
 for i in range(len(encoded_points)):
     x = x_coords[i]
     y = y_coords[i]
-    print(dec_ternary(pyth(x,y)))
+    print(dec_ternary(cantor_pair(x,y)))
